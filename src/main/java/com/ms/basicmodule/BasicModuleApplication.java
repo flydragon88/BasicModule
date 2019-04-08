1c17fcce697299675ddeb96d426d953d50335182
陈邦杰 <bangjie.chen@accenture.com>
2019-04-08 16:09:42 +0800

    First Commit

---
+++ src/main/java/com/ms/basicmodule/BasicModuleApplication.java
@@ -0,0 +1,13 @@
+package com.ms.basicmodule;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BasicModuleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BasicModuleApplication.class, args);
+	}
+
+}
